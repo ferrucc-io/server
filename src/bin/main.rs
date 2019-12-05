99b5a7f83152690a02a9a7b6a8c73fd684052dee
Ferruccio Balestreri <ferruccio.balestreri@intercom.io>
2019-12-05 15:47:27 +0000

    add multi-threads to server and implement graceful shutdown

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,20 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     println!("Listening on: http://127.0.0.1:7878/");
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+           handle_connection(stream);
+        });
     }
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
